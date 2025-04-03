632b454c3bb5fe8a4757e3b6b91f1e202b5e7122
diar2705 <diar.batheesh123@gmail.com>
2025-04-03 18:06:13 +0300

    added staff files for HW1

---
+++ HW1/main.cpp
@@ -0,0 +1,12 @@
+#include "tokens.hpp"
+#include "output.hpp"
+
+int main() {
+    enum tokentype token;
+
+    // read tokens until the end of file is reached
+    while ((token = static_cast<tokentype>(yylex()))) {
+        output::printToken(yylineno, token, yytext);
+    }
+    return 0;
+}
