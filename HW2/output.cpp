662c096abb5413cb75da89914847f77c33a6762c
Yara Zoabi <yarazoabi@campus.technion.ac.il>
2025-05-14 20:42:25 +0300

    fixed bugs

--- HW2/output.cpp
+++ HW2/output.cpp
@@ -1,7 +1,8 @@
 #include "output.hpp"
 #include <iostream>
 
-namespace output {
+namespace output
+{
     static const std::string child_prefix = "├──";
     static const std::string last_child_prefix = "└──";
     static const std::string child_indent = "│   ";
@@ -9,31 +10,35 @@ namespace output {
 
     /* Helper functions */
 
-    static std::string toString(ast::BuiltInType type) {
-        switch (type) {
-            case ast::BuiltInType::INT:
-                return "int";
-            case ast::BuiltInType::BOOL:
-                return "bool";
-            case ast::BuiltInType::BYTE:
-                return "byte";
-            case ast::BuiltInType::VOID:
-                return "void";
-            case ast::BuiltInType::STRING:
-                return "string";
-            default:
-                return "unknown";
+    static std::string toString(ast::BuiltInType type)
+    {
+        switch (type)
+        {
+        case ast::BuiltInType::INT:
+            return "int";
+        case ast::BuiltInType::BOOL:
+            return "bool";
+        case ast::BuiltInType::BYTE:
+            return "byte";
+        case ast::BuiltInType::VOID:
+            return "void";
+        case ast::BuiltInType::STRING:
+            return "string";
+        default:
+            return "unknown";
         }
     }
 
     /* Error handling functions */
 
-    void errorLex(int lineno) {
+    void errorLex(int lineno)
+    {
         std::cout << "line " << lineno << ": lexical error\n";
         exit(0);
     }
 
-    void errorSyn(int lineno) {
+    void errorSyn(int lineno)
+    {
         std::cout << "line " << lineno << ": syntax error\n";
         exit(0);
     }
@@ -42,64 +47,76 @@ namespace output {
 
     PrintVisitor::PrintVisitor() : indents({last_child_indent}), prefixes({last_child_prefix}) {}
 
-    void PrintVisitor::print_indented(const std::string &str) {
-        for (auto it = indents.begin(); it != indents.end() - 1; ++it) {
+    void PrintVisitor::print_indented(const std::string &str)
+    {
+        for (auto it = indents.begin(); it != indents.end() - 1; ++it)
+        {
             std::cout << *it;
         }
         std::cout << prefixes.back() << str << std::endl;
     }
 
-    void PrintVisitor::enter_child() {
+    void PrintVisitor::enter_child()
+    {
         indents.emplace_back(child_indent);
         prefixes.emplace_back(child_prefix);
     }
 
-    void PrintVisitor::enter_last_child() {
+    void PrintVisitor::enter_last_child()
+    {
         indents.emplace_back(last_child_indent);
         prefixes.emplace_back(last_child_prefix);
     }
 
-    void PrintVisitor::leave_child() {
+    void PrintVisitor::leave_child()
+    {
         indents.pop_back();
         prefixes.pop_back();
     }
 
-    void PrintVisitor::visit(ast::Num &node) {
+    void PrintVisitor::visit(ast::Num &node)
+    {
         print_indented("Num: " + std::to_string(node.value));
     }
 
-    void PrintVisitor::visit(ast::NumB &node) {
+    void PrintVisitor::visit(ast::NumB &node)
+    {
         print_indented("NumB: " + std::to_string(node.value));
     }
 
-    void PrintVisitor::visit(ast::String &node) {
+    void PrintVisitor::visit(ast::String &node)
+    {
         print_indented("String: " + node.value);
     }
 
-    void PrintVisitor::visit(ast::Bool &node) {
+    void PrintVisitor::visit(ast::Bool &node)
+    {
         print_indented("Bool: " + std::string((node.value ? "true" : "false")));
     }
 
-    void PrintVisitor::visit(ast::ID &node) {
+    void PrintVisitor::visit(ast::ID &node)
+    {
         print_indented("ID: " + node.value);
     }
 
-    void PrintVisitor::visit(ast::BinOp &node) {
+    void PrintVisitor::visit(ast::BinOp &node)
+    {
         std::string op;
 
-        switch (node.op) {
-            case ast::BinOpType::ADD:
-                op = "+";
-                break;
-            case ast::BinOpType::SUB:
-                op = "-";
-                break;
-            case ast::BinOpType::MUL:
-                op = "*";
-                break;
-            case ast::BinOpType::DIV:
-                op = "/";
-                break;
+        switch (node.op)
+        {
+        case ast::BinOpType::ADD:
+            op = "+";
+            break;
+        case ast::BinOpType::SUB:
+            op = "-";
+            break;
+        case ast::BinOpType::MUL:
+            op = "*";
+            break;
+        case ast::BinOpType::DIV:
+            op = "/";
+            break;
         }
 
         print_indented("BinOp: " + op);
@@ -113,28 +130,30 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::RelOp &node) {
+    void PrintVisitor::visit(ast::RelOp &node)
+    {
         std::string op;
 
-        switch (node.op) {
-            case ast::RelOpType::EQ:
-                op = "==";
-                break;
-            case ast::RelOpType::NE:
-                op = "!=";
-                break;
-            case ast::RelOpType::LT:
-                op = "<";
-                break;
-            case ast::RelOpType::LE:
-                op = "<=";
-                break;
-            case ast::RelOpType::GT:
-                op = ">";
-                break;
-            case ast::RelOpType::GE:
-                op = ">=";
-                break;
+        switch (node.op)
+        {
+        case ast::RelOpType::EQ:
+            op = "==";
+            break;
+        case ast::RelOpType::NE:
+            op = "!=";
+            break;
+        case ast::RelOpType::LT:
+            op = "<";
+            break;
+        case ast::RelOpType::LE:
+            op = "<=";
+            break;
+        case ast::RelOpType::GT:
+            op = ">";
+            break;
+        case ast::RelOpType::GE:
+            op = ">=";
+            break;
         }
 
         print_indented("RelOp: " + op);
@@ -143,25 +162,27 @@ namespace output {
         node.left->accept(*this);
         leave_child();
 
-
         enter_last_child();
         node.right->accept(*this);
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::PrimitiveType &node) {
+    void PrintVisitor::visit(ast::PrimitiveType &node)
+    {
         print_indented("PrimitiveType: " + toString(node.type));
     }
 
-    void PrintVisitor::visit(ast::ArrayType &node) {
-        print_indented("ArrayType: " + toString(node.type) + "[" );
+    void PrintVisitor::visit(ast::ArrayType &node)
+    {
+        print_indented("ArrayType: " + toString(node.type) + "[");
         enter_child();
         node.length->accept(*this);
         print_indented("]");
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::ArrayDereference &node) {
+    void PrintVisitor::visit(ast::ArrayDereference &node)
+    {
         print_indented("ArrayDereference");
 
         enter_child();
@@ -173,7 +194,8 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Cast &node) {
+    void PrintVisitor::visit(ast::Cast &node)
+    {
         print_indented("Cast");
 
         enter_child();
@@ -185,7 +207,8 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Not &node) {
+    void PrintVisitor::visit(ast::Not &node)
+    {
         print_indented("Not");
 
         enter_last_child();
@@ -193,7 +216,8 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::And &node) {
+    void PrintVisitor::visit(ast::And &node)
+    {
         print_indented("And");
 
         enter_child();
@@ -205,7 +229,8 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Or &node) {
+    void PrintVisitor::visit(ast::Or &node)
+    {
         print_indented("Or");
 
         enter_child();
@@ -217,13 +242,18 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::ExpList &node) {
+    void PrintVisitor::visit(ast::ExpList &node)
+    {
         print_indented("ExpList");
 
-        for (auto it = node.exps.rbegin(); it != node.exps.rend(); ++it) {
-            if (it != node.exps.rend() - 1) {
+        for (auto it = node.exps.rbegin(); it != node.exps.rend(); ++it)
+        {
+            if (it != node.exps.rend() - 1)
+            {
                 enter_child();
-            } else {
+            }
+            else
+            {
                 enter_last_child();
             }
             (*it)->accept(*this);
@@ -231,7 +261,8 @@ namespace output {
         }
     }
 
-    void PrintVisitor::visit(ast::Call &node) {
+    void PrintVisitor::visit(ast::Call &node)
+    {
         print_indented("Call");
 
         enter_child();
@@ -243,13 +274,18 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Statements &node) {
+    void PrintVisitor::visit(ast::Statements &node)
+    {
         print_indented("Statements");
 
-        for (auto it = node.statements.begin(); it != node.statements.end(); ++it) {
-            if (it != node.statements.end() - 1) {
+        for (auto it = node.statements.begin(); it != node.statements.end(); ++it)
+        {
+            if (it != node.statements.end() - 1)
+            {
                 enter_child();
-            } else {
+            }
+            else
+            {
                 enter_last_child();
             }
             (*it)->accept(*this);
@@ -257,47 +293,57 @@ namespace output {
         }
     }
 
-    void PrintVisitor::visit(ast::Break &node) {
+    void PrintVisitor::visit(ast::Break &node)
+    {
         print_indented("Break");
     }
 
-    void PrintVisitor::visit(ast::Continue &node) {
+    void PrintVisitor::visit(ast::Continue &node)
+    {
         print_indented("Continue");
     }
 
-    void PrintVisitor::visit(ast::Return &node) {
+    void PrintVisitor::visit(ast::Return &node)
+    {
         print_indented("Return");
 
-        if (node.exp) {
+        if (node.exp)
+        {
             enter_last_child();
             node.exp->accept(*this);
             leave_child();
         }
     }
 
-    void PrintVisitor::visit(ast::If &node) {
+    void PrintVisitor::visit(ast::If &node)
+    {
         print_indented("If");
 
         enter_child();
         node.condition->accept(*this);
         leave_child();
 
-        if (node.otherwise) {
+        if (node.otherwise)
+        {
             enter_child();
-        } else {
+        }
+        else
+        {
             enter_last_child();
         }
         node.then->accept(*this);
         leave_child();
 
-        if (node.otherwise) {
+        if (node.otherwise)
+        {
             enter_last_child();
             node.otherwise->accept(*this);
             leave_child();
         }
     }
 
-    void PrintVisitor::visit(ast::While &node) {
+    void PrintVisitor::visit(ast::While &node)
+    {
         print_indented("While");
 
         enter_child();
@@ -309,29 +355,35 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::VarDecl &node) {
+    void PrintVisitor::visit(ast::VarDecl &node)
+    {
         print_indented("VarDecl");
 
         enter_child();
         node.id->accept(*this);
         leave_child();
 
-        if (node.init_exp) {
+        if (node.init_exp)
+        {
             enter_child();
-        } else {
+        }
+        else
+        {
             enter_last_child();
         }
         node.type->accept(*this);
         leave_child();
 
-        if (node.init_exp) {
+        if (node.init_exp)
+        {
             enter_last_child();
             node.init_exp->accept(*this);
             leave_child();
         }
     }
 
-    void PrintVisitor::visit(ast::Assign &node) {
+    void PrintVisitor::visit(ast::Assign &node)
+    {
         print_indented("Assign");
 
         enter_child();
@@ -343,7 +395,8 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::ArrayAssign &node) {
+    void PrintVisitor::visit(ast::ArrayAssign &node)
+    {
         print_indented("ArrayAssign");
 
         enter_child();
@@ -359,7 +412,8 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Formal &node) {
+    void PrintVisitor::visit(ast::Formal &node)
+    {
         print_indented("Formal");
 
         enter_child();
@@ -371,13 +425,18 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Formals &node) {
+    void PrintVisitor::visit(ast::Formals &node)
+    {
         print_indented("Formals");
 
-        for (auto it = node.formals.rbegin(); it != node.formals.rend(); ++it) {
-            if (it != node.formals.rend() - 1) {
+        for (auto it = node.formals.rbegin(); it != node.formals.rend(); ++it)
+        {
+            if (it != node.formals.rend() - 1)
+            {
                 enter_child();
-            } else {
+            }
+            else
+            {
                 enter_last_child();
             }
             (*it)->accept(*this);
@@ -385,7 +444,8 @@ namespace output {
         }
     }
 
-    void PrintVisitor::visit(ast::FuncDecl &node) {
+    void PrintVisitor::visit(ast::FuncDecl &node)
+    {
         print_indented("FuncDecl");
 
         enter_child();
@@ -405,13 +465,18 @@ namespace output {
         leave_child();
     }
 
-    void PrintVisitor::visit(ast::Funcs &node) {
+    void PrintVisitor::visit(ast::Funcs &node)
+    {
         print_indented("Funcs");
 
-        for (auto it = node.funcs.begin(); it != node.funcs.end(); ++it) {
-            if (it != node.funcs.end() - 1) {
+        for (auto it = node.funcs.begin(); it != node.funcs.end(); ++it)
+        {
+            if (it != node.funcs.end() - 1)
+            {
                 enter_child();
-            } else {
+            }
+            else
+            {
                 enter_last_child();
             }
             (*it)->accept(*this);
---
+++ HW2/parser.tab.h
@@ -0,0 +1,110 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PARSER_TAB_H_INCLUDED
+# define YY_YY_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    VOID = 258,                    /* VOID  */
+    INT = 259,                     /* INT  */
+    BYTE = 260,                    /* BYTE  */
+    BOOL = 261,                    /* BOOL  */
+    TRUE = 262,                    /* TRUE  */
+    FALSE = 263,                   /* FALSE  */
+    RETURN = 264,                  /* RETURN  */
+    IF = 265,                      /* IF  */
+    WHILE = 266,                   /* WHILE  */
+    BREAK = 267,                   /* BREAK  */
+    CONTINUE = 268,                /* CONTINUE  */
+    SC = 269,                      /* SC  */
+    COMMA = 270,                   /* COMMA  */
+    RPAREN = 271,                  /* RPAREN  */
+    LBRACE = 272,                  /* LBRACE  */
+    RBRACE = 273,                  /* RBRACE  */
+    LBRACK = 274,                  /* LBRACK  */
+    RBRACK = 275,                  /* RBRACK  */
+    ID = 276,                      /* ID  */
+    NUM = 277,                     /* NUM  */
+    NUM_B = 278,                   /* NUM_B  */
+    STRING = 279,                  /* STRING  */
+    OR = 280,                      /* OR  */
+    AND = 281,                     /* AND  */
+    NOT = 282,                     /* NOT  */
+    ASSIGN = 283,                  /* ASSIGN  */
+    RELOP_LOW = 284,               /* RELOP_LOW  */
+    RELOP_HIGH = 285,              /* RELOP_HIGH  */
+    BINOP_LOW = 286,               /* BINOP_LOW  */
+    BINOP_HIGH = 287,              /* BINOP_HIGH  */
+    IFX = 288,                     /* IFX  */
+    ELSE = 289,                    /* ELSE  */
+    ID_AS_VALUE = 290,             /* ID_AS_VALUE  */
+    LPAREN = 291,                  /* LPAREN  */
+    CASTING = 292                  /* CASTING  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
